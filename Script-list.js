3f6543da396eeb6daeea526a1002ac77394587a2
KauãDev <kaua26323@gmail.com>
2025-11-11 11:02:56 -0400

    Melhorando e adicionando readme.md ao To-DoList

--- Script-list.js
+++ Script-list.js
@@ -1,60 +1,64 @@
+let h2Element = document.querySelector("#box h2");
+let listElement = document.querySelector("#box ul");
+let inputElement = document.querySelector("#box input");
+let buttonElement = document.querySelector("#box button");
 
+let tasks = JSON.parse(localStorage.getItem("@listaTask")) || [];
 
-let listElement = document.querySelector('#box ul');
-let inputElement = document.querySelector('#box input');
-let buttonElement = document.querySelector('#box button');
+function renderTask() {
+  listElement.innerHTML = "";
 
-let tasks = JSON.parse(localStorage.getItem('@listaTask')) || [];
+  tasks.map((todo) => {
+    let liElement = document.createElement("li");
+    liElement.className = "liElement";
+    let taskText = document.createTextNode(todo);
 
-function renderTask(){
-    listElement.innerHTML = '';
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
 
-    tasks.map((todo) => {
-        let liElement = document.createElement('li');
-        liElement.className = 'liElement';
-        let taskText = document.createTextNode(todo);
+    let linkText = document.createTextNode("Excluir");
+    linkElement.appendChild(linkText);
 
-        let linkElement = document.createElement('a');
-        linkElement.setAttribute('href', '#');
+    let position = tasks.indexOf(todo);
+    linkElement.setAttribute("onclick", `deleteTask(${position})`);
 
-        let linkText = document.createTextNode(" Excluir");
-        linkElement.appendChild(linkText);
+    liElement.appendChild(taskText);
+    liElement.appendChild(linkElement);
+    listElement.appendChild(liElement);
+  });
 
-        let position = tasks.indexOf(todo);
-        linkElement.setAttribute('onclick', `deleteTask(${position})`);
-
-
-        liElement.appendChild(taskText);
-        liElement.appendChild(linkElement);
-        listElement.appendChild(liElement);
-    })
+  if (Array.isArray(tasks) && tasks.length === 0) {
+    h2Element.innerHTML = "Você não tem nenhuma task ainda :(";
+    h2Element.className = "h2Element";
+  } else {
+    h2Element.innerHTML = "";
+  }
 }
 
 renderTask();
 
-function addTask(){
-    if( inputElement.value === ''){
-        alert('Enter a valid task...');
-        return false;
+function addTask() {
+  if (inputElement.value === "") {
+    alert("Enter a valid task...");
+    return false;
+  } else {
+    let newTaks = inputElement.value;
 
-    }else{
-        let newTaks = inputElement.value;
+    tasks.push(newTaks);
+    inputElement.value = "";
 
-        tasks.push(newTaks);
-        inputElement.value = '';
-        
-        renderTask();
-        saveDados();
-    }
+    renderTask();
+    saveDados();
+  }
 }
 buttonElement.onclick = addTask;
 
-function deleteTask(position){
-    tasks.splice(position, 1);
-    renderTask();
-    saveDados();
+function deleteTask(position) {
+  tasks.splice(position, 1);
+  renderTask();
+  saveDados();
 }
 
-function saveDados(){
-    localStorage.setItem('@listaTask', JSON.stringify(tasks));
+function saveDados() {
+  localStorage.setItem("@listaTask", JSON.stringify(tasks));
 }
---
+++ readme.md
@@ -0,0 +1,39 @@
+# 🧑‍💻To-DoList
+
+## 📖 Sobre:
+
+Esse projeto é uma **lista de tarefas (To-Do List)** onde o usuário pode:
+
+1. Cadastrar novas tarefas
+2. Excluir tarefas existentes
+
+O projeto foi criado com o objetivo de **praticar JavaScript**, além de aprimorar o uso de **HTML e CSS**.
+
+## 🚀 Tecnologias utilizadas
+
+- **HTML5**
+- **CSS3**
+- **JavaScript (ES6+)**
+
+## 🎯 Objetivo
+
+Praticar o desenvolvimento front-end utilizando:
+**HTML semântico**, **CSS para estilização e responsividade**, e **JavaScript** para manipulação do DOM e armazenamento de dados no `localStorage`.
+
+## 💡 O que aprendi
+
+Durante o desenvolvimento deste projeto, aprimorei as seguintes habilidades:
+
+- Criar uma estrutura **HTML semântica e organizada**
+- Utilizar **Flexbox** para alinhamento de elementos
+- Aplicar **efeitos de hover** e **transições suaves**
+- Armazenar dados no **Local Storage**
+
+## 🖼️ Prévia do projeto
+
+![alt text](images/image.png)
+![alt text](images/image-1.png)
+
+---
+
+📅 Projeto criado para estudos e prática pessoal.
